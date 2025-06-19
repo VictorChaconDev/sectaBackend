a2d8c50caeec87a5e5ac39ac2eb21e707b0a4272
victorchacon <victor@impulsatalentum.org>
2025-06-19 18:51:05 +0200

    sss

--- src/main/java/Repository/QuestionarioRepository.java
+++ src/main/java/Repository/QuestionarioRepository.java
@@ -1,6 +1,7 @@
 package Repository;
 
 import entity.Questionario;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionarioRepository extends JpaRepository<Questionario, Long> {
